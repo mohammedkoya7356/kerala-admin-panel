3720230094adae54da7957a79361173111ed5157
mohammedkoya7356 <muhammadkoya04@gmail.com>
2025-07-17 23:36:19 +0530

    Fix package.json and add build script

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/kerala-admin-panel/' // 👈 important if deploying to GitHub Pages
+});
